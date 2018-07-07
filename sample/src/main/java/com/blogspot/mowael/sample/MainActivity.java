79d5b0a26405222aefd5aaf103bb0b0b14a89001
Mohamed Wael <mohamed.wael@v-apps.co>
2018-07-07 16:45:42 +0200

    upgrading gradle and adding sample app

---
+++ sample/src/main/java/com/blogspot/mowael/sample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.blogspot.mowael.sample;
+
+import android.os.Bundle;
+
+import com.blogspot.mowael.molib.activities.MoActivity;
+
+public class MainActivity extends MoActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
